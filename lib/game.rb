2155c046d0622caac4f1fc1a8ee0a1c4b32809dd
DennisNeu <76845672+DennisNeu@users.noreply.github.com>
2024-07-06 20:00:50 +0200

    Add method to get player choice, cleanup

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,27 @@ class Game
     @color_array = %w[blue red green yellow magenta white black grey]
     @ui_handler = UiHandler.new
     @guesser = Guesser.new(sequence_length, @color_array)
-    @sequence_array = guesser.generate_sequence
+    @sequence_array = @guesser.generate_sequence
+    @tries = tries
+  end
+
+  # private
+
+  def get_player_choice
+    @ui_handler.print_text("To make a choice, enter four of the shown numbers seperated by spaces")
+    while true
+      input = gets.chomp
+      puts input
+      input_array = input.split(" ").map(&:to_i)
+      p input_array
+      break if check_array(input_array)
+    end
+    input_array
+  end
+
+  def check_array(array)
+    return false unless array.is_a?(Array) && array.length == 4
+
+    array.all? { |element| element.is_a?(Integer) && element.between?(1, 8) }
   end
 end
--- lib/ui_handler.rb
+++ lib/ui_handler.rb
@@ -1,24 +1,25 @@
 require "colorize"
 
 class UiHandler
-  def print_introduction(color_array)
+  def print_introduction(color_array, tries)
     print_seperator_new_line
 
     puts "This is a game of mastermind"
     puts "The computer will randomly determine a sequence of four of the following colors:"
-    puts print_colors(color_array)
+    puts print_all_colors(color_array)
+    puts ""
+    puts "You have to guess the correct sequence. You have #{tries.to_s.colorize(:red)} tries!"
 
     print_seperator_new_line
   end
 
-  def print_colors(color_array)
+  def print_all_colors(color_array)
     string = ""
-    color_array.each { |color| string.concat("#{color.colorize(color.to_sym)} ") }
+    color_array.each_with_index { |color, index| string.concat("#{index + 1}: #{color.colorize(color.to_sym)} ") }
     string
   end
 
   def print_text(string)
-    print_seperator_new_line
     puts string
     print_seperator_new_line
   end
